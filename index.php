0df726f02d7edf2b32b92f82ced249232bac0900
Nikita202512 <nikita_egorov_2016@mail.ru>
2025-04-10 11:44:29 +0300

    Мой второй коммит

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
-version: '3.8'
-services:
+<?php
 
-  back_nikita:
-    image: yiisoftware/yii2-php:8.1-fpm
-    container_name: back_nikita_1
-    volumes:
-      - ./my_php_nikita:/var/www/my_php_nikita
-      
-  nginx_nikita:
-    image: nginx:latest
-    container_name: nginx_nikita
-    ports:
-      - 80:80
-    volumes:
-      - ./.logs/nginx:/var/log/nginx
-      - ./.docker/nginx/conf.d:/etc/nginx/conf.d
-    depends_on:
-      - back_nikita
+
+ class myMozyr
+ {
+   const PI = 3.14;
+   public static $bar = 'Mozyr';
+
+   public $bbb = 555;
+  
+   public static function bca() {
+     $obj = new self();
+     echo $obj->bbb;
+   }
+ }
